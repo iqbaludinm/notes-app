10d9e204895cb7506ab7f4c98d7b720acd686137
Johan <57840332+johannasendi@users.noreply.github.com>
2022-08-14 01:25:24 +0800

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title>{{ config('app.name', 'Laravel') }}</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Johan Nasendi">
+        <meta name="description" content="Notes APP Documentation API">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>NOTES APP</title>
 
        <!--GOOGLE FONTS-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,10 +15,10 @@
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700;800;900&family=Source+Sans+3:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
         <!--FONT AWESOME-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
         <!--EXTERNAL STYLE SHEET-->
-        <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+        <link  href="{{asset('/css/style.css')}}" rel="stylesheet">
     </head>
     <body>
         <div class="content">
